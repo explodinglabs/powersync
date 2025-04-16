5333ba66784f5f96aaae2e587ef4c05378a370d2
Beau Barker <beau@explodinglabs.com>
2025-04-17 09:43:53 +1000

    Improvements

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-Include in a webpage:
+Start ssehub:
+
+```sh
+docker run --detach --name ssehub --publish 8080:8080 ghcr.io/explodinglabs/ssehub
+```
+
+Start RabbitMQ:
+
+```sh
+docker run --detach --name rabbitmq --publish 5672:5672 --publish 15672:15672 rabbitmq:3-management
+```
+
+Include in your webpage:
+
 ```html
-<script type="text/javascript" src="https://github.com/explodinglabs/refresh.js/refresh.js"></script>
+<script
+  type="text/javascript"
+  src="https://github.com/explodinglabs/refresh.js/refresh.js"
+></script>
 ```
--- refresh.js
+++ refresh.js
@@ -1,5 +1,41 @@
 var changesEventSource = new EventSource("/events/changes");
 
+function isExternal(url) {
+  var match = url.match(
+    /^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/
+  );
+  if (
+    typeof match[1] === "string" &&
+    match[1].length > 0 &&
+    match[1].toLowerCase() !== location.protocol
+  )
+    return true;
+  if (
+    typeof match[2] === "string" &&
+    match[2].length > 0 &&
+    match[2].replace(
+      new RegExp(
+        ":(" + { "http:": 80, "https:": 443 }[location.protocol] + ")?$"
+      ),
+      ""
+    ) !== location.host
+  )
+    return true;
+  return false;
+}
+
+function refreshCss() {
+  let rnd = (Math.random() + 1).toString(36).substring(7);
+  var links = document.getElementsByTagName("link");
+  for (var cl in links) {
+    var link = links[cl];
+    if (link.rel === "stylesheet" && !isExternal(link.href)) {
+      // console.log(link.href);
+      link.href = link.href.replace(/\?(.*)/, "") + "?" + rnd;
+    }
+  }
+}
+
 changesEventSource.onopen = (event) => {
   console.log("changesEventSource open");
 };
@@ -17,12 +53,5 @@ changesEventSource.addEventListener("js", (event) => {
 });
 
 changesEventSource.addEventListener("css", (event) => {
-  var links = document.getElementsByTagName("link");
-  for (var cl in links) {
-    var link = links[cl];
-    if (link.rel === "stylesheet") {
-      let rnd = (Math.random() + 1).toString(36).substring(7);
-      link.href = link.href.replace(/\?(.*)/, "") + "?" + rnd;
-    }
-  }
+  refreshCss();
 });
